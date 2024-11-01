dd78052b5f17f343cc30f2a6e4e195e4ae16dfee
vitorVier1 <vitor.vier@sou.unijui.edu.br>
2024-11-01 16:06:07 -0300

    add-outTable

--- Plantel/transferencias.py
+++ Plantel/transferencias.py
@@ -18,11 +18,11 @@ print("\nTransferências da Temporada 2023/24\n")
 
 print("\nENTRADAS")
 # Criando tabela
-table = PrettyTable()
-table.field_names = ["#", "Jogadores", "Posição", "Idade", "Nacionalidade", "Origem", "Valor de Mercado (na época)", "Valor Pago"]
+table_entradas = PrettyTable()
+table_entradas.field_names = ["#", "Jogadores", "Posição", "Idade", "Nacionalidade", "Origem", "Valor de Mercado (na época)", "Valor Pago"]
 
-jogadores = soup.select('table[class="items"] tbody tr')
-for jogador in jogadores:
+jogadores_in = soup.select('table[class="items"] tbody tr')
+for jogador in jogadores_in:
     dados_jogador = [td.text.strip() for td in jogador.find_all('td') if td.text.strip()]
     
     if len(dados_jogador) >= 9:  # Verifica se existem dados suficientes
@@ -40,7 +40,41 @@ for jogador in jogadores:
         valorPago = dados_jogador[10]
         
         # Adicionando dados à tabela
-        table.add_row([num, nome, posicao, idade, nacionalidade + nacionalidade2, origem, valorJogador, valorPago])
+        table_entradas.add_row([num, nome, posicao, idade, nacionalidade + nacionalidade2, origem, valorJogador, valorPago])
 
 # Exibindo tabela
-print(table)
+print(table_entradas)
+
+print('\n-------------------------------------------------------------------------------------------------------------------------------------------------------------\n')
+
+print("SAÍDAS")
+urlOut = "https://www.transfermarkt.com.br/real-madrid-cf/rekordabgaenge/verein/418/saison_id/2023/pos//detailpos/0/w_s//plus/1"
+responseOut = requests.get(urlOut, headers = headers)
+soupOut = BeautifulSoup(responseOut.content, "html.parser")
+
+table_saidas = PrettyTable()
+table_saidas.field_names = ["#", "Jogadores", "Posição", "Idade", "Nacionalidade", "Destino", "Valor de Mercado (na época)", "Valor Pago"]
+
+jogadores_out = soupOut.select('table[class="items"] tbody tr')
+for jogador in jogadores_out:
+    dados_jogador = [td.text.strip() for td in jogador.find_all('td') if td.text.strip()]
+    
+    if len(dados_jogador) > 10:  # Verifica se existem dados suficientes
+        num = dados_jogador[0]
+        nome = dados_jogador[2]
+        posicao = dados_jogador[3]
+        idade = dados_jogador[4]
+
+        nacionalidade_imgs = jogador.select('td img[alt]')
+        nacionalidade = ''.join([img['alt'] for img in nacionalidade_imgs][1]) if len(nacionalidade_imgs) < 6 else ''.join([img['alt'] for img in nacionalidade_imgs][1]) + "/"
+        nacionalidade2 = ''.join([img['alt'] for img in nacionalidade_imgs][2]) if len(nacionalidade_imgs) >= 6 else ''
+
+        origem = dados_jogador[6].split('\n')[0]
+        valorJogador = dados_jogador[9]
+        valorPago = dados_jogador[10]
+        
+        # Adicionando dados à tabela
+        table_saidas.add_row([num, nome, posicao, idade, nacionalidade + nacionalidade2, origem, valorJogador, valorPago])
+
+# Exibindo tabela
+print(table_saidas)
